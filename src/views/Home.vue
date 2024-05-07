f9afad51f17ce9eaee48fd697df1f8a27a8662e0
Josh Emberton <josh.emberton@gmail.com>
2024-05-07 19:34:38 -0400

    nightly: added database for blog posts

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,4 +1,9 @@
 <script setup lang="ts">
+
+import blogData from '../../public/database.json'
+
+console.log(blogData)
+
 </script>
 
 <template>
@@ -9,6 +14,10 @@
         <p>I am currently working on this site ... things may be broken and incomplete.</p>
         <p>I am planning to add my projects, interests, and general knowledge of experiences here.</p>
     </div>
+    <template v-for="post in blogData.posts">
+        <div>{{ post.title }}</div>
+        <div>{{ post.body }}</div>
+    </template>
 </div>
 </template>
 
