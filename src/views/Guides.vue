49a63dd5fa4abb70ca7d70ff398d72fbfd4e59a6
Josh Emberton <josh.emberton@gmail.com>
2024-06-23 11:06:47 -0400

    fixed TOC Highlight for on screen elements. Needs moved to lib/index.ts, but working for now

--- src/views/Guides.vue
+++ src/views/Guides.vue
@@ -19,7 +19,6 @@ import { emojify } from 'node-emoji'
 const pageContent: Ref<HTMLElement | null> = ref(null)
 const pageData = ref("")
 const tocContent = ref<TOCLink[]>([])
-const tocHighlight = ref("")
 
 const generateTab = (level: number) => {
     let tabSpace = ""
@@ -59,43 +58,40 @@ const buildPage = async (category: string, page: string) => {
 // FIXME move to lib/index.ts
 const tocHighlightHandler = () => {
     if (globalState.windowSize.width < 1280) return
-    if (window.scrollY === 0) { tocHighlight.value = ""; return }
 
     let tocLinks = document.querySelectorAll<HTMLAnchorElement>('a[type=toc]')
     let headings: { id: string, top: number }[] = []
 
-    Array.from(tocLinks).forEach((link: HTMLAnchorElement) => {
+    let temp = ""
+
+    Array.from(tocLinks).reverse().forEach((link: HTMLAnchorElement) => {
         let heading = document.getElementById(link.id.slice(5))
         let top = heading?.getBoundingClientRect().top || 0
         let font = parseFloat(window.getComputedStyle(link, null).getPropertyValue('font-size'))
 
+        if (window.scrollY !== 0) {
+            if (top > 0 && top < globalState.windowSize.height) {
+                link.classList.remove("bg-base", "text-mauve")
+                link.classList.add("bg-mantle")
+            } else {
+                link.classList.remove("bg-base", "text-mauve", "bg-mantle")
+            }
+
+            if (top > 0 && top < 180) {
+                link.classList.remove("bg-mantle")
+                link.classList.add("bg-base", "text-mauve")
+                temp = link.id.slice(5)
+            } else if (top < 180 && temp === "") {
+                link.classList.remove("bg-mantle")
+                link.classList.add("bg-base", "text-mauve")
+                temp = link.id.slice(5)
+            }
+        } else {
+            link.classList.remove("bg-base", "text-mauve", "bg-mantle")
+        }
+
         headings.push({ id: link.id.slice(5), top: top + font })
     })
-
-    let temp = ""
-
-    for (const heading of headings.reverse()) {
-        if (heading.top > 0 && heading.top < 180) {
-            tocHighlight.value = heading.id
-            temp = heading.id
-        } else if (heading.top < 180 && temp === "") {
-            tocHighlight.value = heading.id
-            temp = heading.id
-        }
-    }
-}
-
-const isOnScreen = (id: string) => {
-    if (window.scrollY === 0) return false
-
-    let heading = document.getElementById(id)
-
-    if (heading) {
-        let top = heading.getBoundingClientRect().top
-        if (top > 0 && top < globalState.windowSize.height) return true
-    }
-
-    return false
 }
 
 onMounted(() => {
@@ -137,7 +133,6 @@ watch(() => pageData.value, () => {
         <template v-for="tocLink in tocContent">
             <a
                 class="text-sm font-retina gap-xs flex row p-xxs hover-green"
-                :class="tocHighlight === tocLink.url ? 'text-mauve bg-base' : isOnScreen(tocLink.url) ? 'bg-mantle' : ''"
                 :href="`#${ tocLink.url }`"
                 type="toc"
                 :id="`href#${ tocLink.url }`"
